479ee901ccddcd31af5a14de0e97c4d2963b7566
Eric tyrrell <eric.tyrrell18+github@gmail.com>
2025-10-28 23:06:46 -0400

    move migrate.js to ts
            avoid logging the database url

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,11 +1,9 @@
-const { drizzle } = require("drizzle-orm/postgres-js");
-const { migrate } = require("drizzle-orm/postgres-js/migrator");
-const postgres = require("postgres");
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
 
 const runMigration = async () => {
   if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
-
-  console.log(process.env.DATABASE_URL);
 
   const sql = postgres(process.env.DATABASE_URL, { max: 1 });
   const db = drizzle(sql);
